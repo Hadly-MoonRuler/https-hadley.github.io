3bed8221b070ecb9f550dd45b5e2159feb3dc70a
yeatom <l980831@163.com>
2024-05-28 17:06:21 +0800

    some parts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <BrowserRouter>
+                <div>
+                    <Navbar/>
+                    <Routes>
+                        <Route path="/" element={<Home/>}></Route>
+                        <Route path="*" element={<div>404 Not Found</div>}></Route>
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import AboutMe from "./AboutMe";
+import MySkills from "./MySkills";
+
+export default function Home() {
+    return (
+        <>
+            <HeroSection />
+            <MySkills />
+            <AboutMe />
+            {/*<MyPortfolio />*/}
+            {/*<Testimonial />*/}
+            {/*<ContactMe />*/}
+            {/*<Footer />*/}
+        </>
+    );
+}
