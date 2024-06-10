b8e7ac7bed3977c43075bf74498519d1582f7ddd
yeatom <l980831@163.com>
2024-06-10 17:33:30 +0800

    add a line

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
+interface LanguageContextType {
+    language: 'en' | 'zh' | '';
+    toggleLanguage: () => void;
+}
+
 function App() {
+    const LanguageContext = createContext();
+
     return (
         <div className="App">
             <BrowserRouter>
